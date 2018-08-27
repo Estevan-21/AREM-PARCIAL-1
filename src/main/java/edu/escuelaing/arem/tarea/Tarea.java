1aa73553aa7e7e4a52c6ee7f159fae5c792bff97
Estevan-21 <bravar06@hotmail.com>
2018-08-26 20:16:21 -0500

    Tarea 2 terminada

--- src/main/java/edu/escuelaing/arem/tarea/Tarea.java
+++ src/main/java/edu/escuelaing/arem/tarea/Tarea.java
@@ -1,4 +1,4 @@
-package edu.escuelaing.arem.tarea;
+    package edu.escuelaing.arem.tarea;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
@@ -7,6 +7,8 @@ import java.io.IOException;
 import static java.lang.Math.sqrt;
 import java.util.ArrayList;
 import java.util.List;
+import spark.Request;
+import spark.Response;
 import static spark.Spark.*;
 
 
@@ -27,28 +29,69 @@ public class Tarea {
      * @throws  java.io.IOException excepcion
      */
     public static void main(String[] args) {
-        //leer("C:/Users/Estevan/Desktop/archivo2.txt");      
-        leer();
+        //leer("C:/Users/Estevan/Desktop/archivo2.txt");              
         port(getPort());
-        get("/lab02", (req, res) -> "La media de los datos encontrados en el archivo es: "+calculoMedia(lista)+"\nLa desviacion estandar de los datos encontrados en el archivo es: "+calculoDesviacion(lista) );        
+        get("/lab02", (req, res) -> inputDataPage(req, res));
+        get("/results", (req, res) -> resultsPage(req, res));
     }
-    /**
-     * Lee los números de una lista y los almacena en una lista enlazada
-     * 
-     * 
-     */
-    public static void leer(){
-        for(int i=0;i<numeros.length;i++){
-            lista.addLast(numeros[i]);
+
+    private static String inputDataPage(Request req, Response res) {
+        String pageContent
+                = "<!DOCTYPE html>"
+                + "<html>"
+                + "<body>"
+                + "<h1>CALCULADORA DE MEDIA Y DESVIACION ESTANDAR</h1>"
+                + "<h2>AREM 2018-2</h2>"
+                + "<h3>Por favor ingrese los números a ser procesados</h3>"
+                + "<form action=\"/results\">"
+                + "  Ingrese los números separados por un espacio:<br>"
+                + "  <input type=\"text\" name=\"firstname\" value=\"\">"
+                + "  <br>"
+                + "  <br> <br>"                  
+                + "  <input type=\"submit\" value=\"Enviar\">"
+                + "</form>"
+                + "<p>Al hacer click en el botón \"Enviar\",sus datos serán procesados y será redirigido a la página \"/results\".</p>"
+                + "</body>"
+                + "</html>";
+        return pageContent;
+    }
+
+    private static String resultsPage(Request req, Response res) {        
+        String[] arre=req.queryParams("firstname").split("\\s+");
+        for(int a=0;a<arre.length;a++){
+            lista.addLast(Float.parseFloat(arre[a]));
         }
+        String pageContent
+                = "<!DOCTYPE html>"
+                + "<html>"
+                + "<body>"                
+                + "<h2>AREM 2018-2</h2>"
+                + "<h3>La media de los números ingresados es:</h3>"
+                + calculoMedia(lista)
+                + "<h3>La desviación estandar de los números ingresados es:</h3>"
+                + calculoDesviacion(lista)                
+                + "</body>"
+                + "</html>";
+        return pageContent;
     }
-    
+
+    /**
+     * This method reads the default port as specified by the PORT variable in
+     * the environment.
+     *
+     * Heroku provides the port automatically so you need this to run the
+     * project on Heroku.
+     */
     static int getPort() {
         if (System.getenv("PORT") != null) {
             return Integer.parseInt(System.getenv("PORT"));
         }
-        return 4567; //returns default port if heroku-port isn't set(i.e. on localhost)
+        return 4567; //returns default port if heroku-port isn't set (i.e. on localhost)
     }
+
+
+    
+    
     /**
      * Lee los números de un archivo .txt y los almacena en una lista enlazada
      * @throws java.io.IOException IoException
